b6bd75a3a55fa2093d728243f425e42a374479dd
Victor Melo <victorvmelo@gmail.com>
2019-07-14 14:14:22 -0300

    Criei o form de pesquisa
    mas nao coloquei no html ainda

--- administer/administer/funcionarios/forms.py
+++ administer/administer/funcionarios/forms.py
@@ -11,3 +11,8 @@ class funcionario_form(FlaskForm ):
 	email = StringField("Email", validators=[DataRequired(message="Esse campo e obrigatorio"), Length(min=3, max=120, message="Tamanho minimo de 3 caracteres e maximo de 120!"), Email()])
 	setor = SelectField("Setor", validators=[DataRequired(message="Esse campo e obrigatorio")], choices=[("0", "Equipe administrativo"), ("1", "Desenvolvedor"), ("2", "Equipe projetos"), ("3", "Equipe RH"), ("4", "Equipe marketing"), ("5", "Equipe presidencia"), ("6", "Equipe Negocios")])
 	submit = SubmitField("Novo Funcionario")
+
+class pesquisa_func_form(FlaskForm):
+
+	pesquisa = StringField("Digite o nome do funcionario")
+	submit = SubmitField("Pesquisar")
